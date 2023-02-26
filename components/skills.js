6b563fa3d091e62bcc04b6441bf01971fc341ace
jinareum <jinareum.76@gmail.com>
2023-02-27 02:12:11 +0900

    feat : Skill Component추가

---
+++ components/skills.js
@@ -0,0 +1,13 @@
+import styled from  '@emotion/styled'
+
+export const Skills = styled.span`
+  padding:0.2em 0.5em;
+  font-size: 0.8em;
+  line-height: 1.3;
+  color:#9065b0;
+  background-color:rgba(135,131,120,0.2);
+  border-radius: 3px;
+    
+`
+
+export default Skills
