db98981ab8efc7e5ee13956bad3bd8bbdef796f7
jinareum <jinareum.76@gmail.com>
2023-02-28 22:47:44 +0900

    chore : 404 텍스트 수정

--- pages/404.js
+++ pages/404.js
@@ -12,11 +12,11 @@ const NotFound = () => {
   return (
     <Container>
       <Heading as="h1">Not found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
+      <Text>페이지를 찾을 수 없습니다. :(</Text>
       <Divider my={6} />
       <Box my={6} align="center">
         <Button as={NextLink} href="/" colorScheme="teal">
-          Return to home
+          메인으로 돌아가주세요
         </Button>
       </Box>
     </Container>
