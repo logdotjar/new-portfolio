b44e87e9ddde1dce20ad0a9d6d14728a87f72bb7
jinareum <jinareum.76@gmail.com>
2023-04-18 14:00:36 +0900

    style : footer style 여백 추가

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Box align="center" opacity={0.6} fontSize="sm">
+    <Box marginTop="50px" padding="10px 0" align="center" opacity={0.6} fontSize="sm">
       &copy; {new Date().getFullYear()} Jinareum. All Rights Reserved.
     </Box>
   )
