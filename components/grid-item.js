51eb5d93da1e097e481e2011e0e290ae18cf22a2
jinareum <jinareum.76@gmail.com>
2023-04-18 14:00:22 +0900

    feat : 레이아웃 함수 생성

--- components/grid-item.js
+++ components/grid-item.js
@@ -44,7 +44,29 @@ export const WorkGridItem = ({ children, id, title, thumbnail }) => (
     </LinkBox>
   </Box>
 )
-
+export const ProjectGridItem = ({ children, id, title, thumbnail }) => (
+    <Box w="100%" textAlign="center">
+        <LinkBox
+            as={NextLink}
+            href={`/sides/${id}`}
+            scroll={false}
+            cursor="pointer"
+        >
+            <Image
+                src={thumbnail}
+                alt={title}
+                className="grid-item-thumbnail"
+                placeholder="blur"
+            />
+            <LinkOverlay as="div" href={`/sides/${id}`}>
+                <Text mt={2} fontSize={20}>
+                    {title}
+                </Text>
+            </LinkOverlay>
+            <Text fontSize={14}>{children}</Text>
+        </LinkBox>
+    </Box>
+)
 export const GridItemStyle = () => (
   <Global
     styles={`
